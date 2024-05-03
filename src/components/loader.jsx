62d27d17a644b097da8366a62cc1e9e53ddb6407
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-05-03 22:16:35 +0200

    Update loader.jsx

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -4,8 +4,8 @@ const Loader = () => {
     return <div className='backdropL'>
         <div className="loader relative">
             <p className='loader-text text-2xl absolute left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%] text-[#f1ccae]'>
-                {/* 3DGrapher */}
-                Loader v.0.1.3 by cyanidium
+                3DGrapher
+                {/* Loader v.0.1.3 by cyanidium */}
             </p>
         </div>
     </div>;
