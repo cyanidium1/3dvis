94deb3a07c8c293f7bd5b5eb1eb72448b510c5b5
@anasty2234 <a.trunova@pinta.pro>
2024-05-19 20:43:51 +0300

    add contacts page

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -4,52 +4,82 @@ import { useState, useRef } from "react";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 const Carousel = ({ portfolioPosts }) => {
+  console.log(portfolioPosts);
 
-    console.log(portfolioPosts)
+  const [activeIndex, setActiveIndex] = useState(0);
+  const sliderRef = useRef(null);
 
-    const [activeIndex, setActiveIndex] = useState(0);
-    const sliderRef = useRef(null);
-
-    const handlePrevClick = () => {
-        setActiveIndex((prevIndex) => (prevIndex === 0 ? portfolioPosts.length - 1 : prevIndex - 1));
-        sliderRef.current.prepend(sliderRef.current.lastChild);
-    };
-
-    const handleNextClick = () => {
-        setActiveIndex((prevIndex) => (prevIndex === portfolioPosts.length - 1 ? 0 : prevIndex + 1));
-        sliderRef.current.append(sliderRef.current.firstChild);
-    };
-
-    const truncateDescription = (description) => {
-        if (description.length > 100) {
-            return description.slice(0, 100) + "...";
-        } else {
-            return description;
-        }
-    };
-
-    return (
-        <div className="animBox h-screen overflow-hidden">
-            <div className="slider" ref={sliderRef}>
-                {portfolioPosts && portfolioPosts.map((el, index) => (
-                    <div className={`item ${index === activeIndex ? "active" : ""}`} key={index}>
-                        <img className="carousel-photo" src={portfolioPosts[index].coverImage.url} loading="eager" alt="" />
-                        <div className="content w-[40vw] md:w-96 left-2 md:left-8  bg-dark-brown bg-opacity-50 p-2 md:p-6">
-                            <h2 className="playFairFont text-xl md:text-5xl my-1 md:my-0">{el.title}</h2>
-                            <p className="font-extralight text-xs md:text-lg uppercase tracking-widest flex items-center">{truncateDescription(el.description)}</p>
-                            <Link href={`/portfolio/${el.slug}`}>
-                                <p className="btn justify-center bg-brown text-white text-center uppercase px-6 py-4 text-xs w-full md:w-fit">Read more</p>
-                            </Link>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <nav className="nav">
-                <button onClick={handlePrevClick} className="btn prev" name="arrow-back-outline"><SlArrowLeft /></button>
-                <button onClick={handleNextClick} className="btn next" name="arrow-forward-outline"><SlArrowRight /></button>
-            </nav>
-        </div>
+  const handlePrevClick = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? portfolioPosts?.length - 1 : prevIndex - 1
     );
+    sliderRef.current.prepend(sliderRef.current.lastChild);
+  };
+
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === portfolioPosts?.length - 1 ? 0 : prevIndex + 1
+    );
+    sliderRef.current.append(sliderRef.current.firstChild);
+  };
+
+  const truncateDescription = (description) => {
+    if (description.length > 100) {
+      return description.slice(0, 100) + "...";
+    } else {
+      return description;
+    }
+  };
+
+  return (
+    <div className="animBox h-screen overflow-hidden">
+      <div className="slider" ref={sliderRef}>
+        {portfolioPosts &&
+          portfolioPosts.map((el, index) => (
+            <div
+              className={`item ${index === activeIndex ? "active" : ""}`}
+              key={index}
+            >
+              <img
+                className="carousel-photo"
+                src={portfolioPosts[index].coverImage.url}
+                loading="eager"
+                alt=""
+              />
+              <div className="content w-[40vw] md:w-96 left-2 md:left-8  bg-dark-brown bg-opacity-50 p-2 md:p-6">
+                <h2 className="playFairFont text-xl md:text-5xl my-1 md:my-0">
+                  {el.title}
+                </h2>
+                <p className="font-extralight text-xs md:text-lg uppercase tracking-widest flex items-center">
+                  {truncateDescription(el.description)}
+                </p>
+                <Link href={`/portfolio/${el.slug}`}>
+                  <p className="btn justify-center bg-brown text-white text-center uppercase px-6 py-4 text-xs w-full md:w-fit">
+                    Read more
+                  </p>
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+      <nav className="nav">
+        <button
+          onClick={handlePrevClick}
+          className="btn prev"
+          name="arrow-back-outline"
+        >
+          <SlArrowLeft />
+        </button>
+        <button
+          onClick={handleNextClick}
+          className="btn next"
+          name="arrow-forward-outline"
+        >
+          <SlArrowRight />
+        </button>
+      </nav>
+    </div>
+  );
 };
 
 export default Carousel;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,22 +1,194 @@
 import Container from "@/components/container";
 import Layout from "@/components/layout";
-import { Input } from "@nextui-org/react";
+import { Box, Button, Flex, Input, Text, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+
+import {
+  FaTwitter,
+  FaYoutube,
+  FaLinkedinIn,
+  FaInstagram,
+  FaFacebookF,
+} from "react-icons/fa";
 
 export default function Services() {
-    return (
-        <Layout>
-            <main className=" h-screen">
-                <Container>
-                    <div className="flex justify-center items-center">
-                        <form action="">
-                            <Input type="email" variant="underlined" label="Email" />
-                            <Input type="email" variant="underlined" label="Email" />
-                            <Input type="email" variant="underlined" label="Email" />
+  const [formData, setFormData] = useState({
+    fullName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    message: "",
+  });
 
-                        </form>
-                    </div>
-                </Container>
-            </main>
-        </Layout>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    const newValue = name === "phoneNumber" ? value.replace(/\D/g, "") : value;
+
+    setFormData({ ...formData, [name]: newValue });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const stylingSocials = {
+    w: "36px",
+    h: "36px",
+    borderRadius: "50%",
+    color: "#957f72",
+    border: "1px solid #957f72",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return (
+    <Layout>
+      <Flex bgColor={"#f9f1ec"} justify={"center"} alignItems={"center"}>
+        <Container>
+          <Flex
+            py={"96px"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            h={"100%"}
+            w={"100%"}
+          >
+            <div className="flex flex-col md:flex-row  ">
+              <Flex flexDir={"column"}>
+                <Flex gap={"8px"} alignItems={"center"}>
+                  <Text className="text-mobile-lg sm:text-7xl lg:text-9xl text-[#4c4037]">
+                    &mdash;
+                  </Text>
+                  <Text className="text-mobile-lg sm:text-9xl lg:text-xxl text-[#4c4037]">
+                    Get in touch
+                  </Text>
+                </Flex>
+
+                <Text
+                  fontSize={"18px"}
+                  color="#957f72"
+                  fontWeight={300}
+                  maxW={"512px"}
+                  mt={"20px"}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in eros elementum tristique. Duis
+                  cursus, mi quis viverra ornare.
+                </Text>
+              </Flex>
+              <Flex justifyContent={"flex-end"} flexDir={"column"} gap={"8px"}>
+                <div className="mt-5  md:mt-0 ">
+                  <Text
+                    alignSelf={"flex-start"}
+                    color={"#957f72"}
+                    fontSize={"16px"}
+                  >
+                    Follow us:
+                  </Text>
+                  <Flex gap={"10px"}>
+                    <Flex {...stylingSocials}>
+                      <FaFacebookF />
+                    </Flex>
+                    <Flex {...stylingSocials}>
+                      <FaTwitter />
+                    </Flex>
+                    <Flex {...stylingSocials}>
+                      <FaYoutube />
+                    </Flex>
+                    <Flex {...stylingSocials}>
+                      <FaLinkedinIn />
+                    </Flex>
+                    <Flex {...stylingSocials}>
+                      <FaInstagram />
+                    </Flex>
+                  </Flex>
+                </div>
+              </Flex>
+            </div>
+          </Flex>
+
+          <Flex
+            as={"form"}
+            p={"80px 96px"}
+            w={"100%"}
+            h={"auto"}
+            bgColor={"white"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            border={"1px solid #e6e6e6"}
+            gap={"50px"}
+            onSubmit={handleSubmit}
+          >
+            <Input
+              p={"16px 32px"}
+              border={"1px solid #e6e6e6"}
+              w={"100%"}
+              placeholder="Full Name"
+              _placeholder={{ fontSize: "18px" }}
+              fontSize={"18px"}
+              color={" #b6a497"}
+              _hover={{ border: "1px solid #b6a497" }}
+              _focus={{ border: "1px solid #b6a497" }}
+              name="fullName"
+              onChange={handleInputChange}
+              required
+            />
+            <Input
+              p={"16px 32px"}
+              border={"1px solid #e6e6e6"}
+              w={"100%"}
+              placeholder="Email Address"
+              _placeholder={{ fontSize: "18px" }}
+              fontSize={"18px"}
+              color={" #b6a497"}
+              _hover={{ border: "1px solid #b6a497" }}
+              _focus={{ border: "1px solid #b6a497" }}
+              name="emailAddress"
+              onChange={handleInputChange}
+            />
+            <Input
+              p={"16px 32px"}
+              required
+              border={"1px solid #e6e6e6"}
+              w={"100%"}
+              placeholder="Phone Number"
+              _placeholder={{ fontSize: "18px" }}
+              fontSize={"18px"}
+              color={" #b6a497"}
+              _hover={{ border: "1px solid #b6a497" }}
+              _focus={{ border: "1px solid #b6a497" }}
+              name="phoneNumber"
+              onChange={handleInputChange}
+              value={formData.phoneNumber}
+            />
+            <Textarea
+              placeholder="Message..."
+              minH="176px"
+              p={"16px 32px"}
+              border={"1px solid #e6e6e6"}
+              w={"100%"}
+              _placeholder={{ fontSize: "18px" }}
+              fontSize={"18px"}
+              color={" #b6a497"}
+              _hover={{ border: "1px solid #b6a497" }}
+              _focus={{ border: "1px solid #b6a497" }}
+              name="message"
+              onChange={handleInputChange}
+            />
+            <Flex mr="auto" color={"white"}>
+              <Button
+                maxW={"90px"}
+                h={"48px"}
+                bgColor={"#4c4037"}
+                p="16px 24px"
+                type="submit"
+              >
+                Send
+              </Button>
+            </Flex>
+          </Flex>
+        </Container>
+      </Flex>
+    </Layout>
+  );
 }
