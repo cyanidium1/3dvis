17a3840689e0f76162422ffc518b3487b1803672
@anasty2234 <a.trunova@pinta.pro>
2024-05-25 23:55:13 +0300

    add content from server services

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -5,19 +5,136 @@ import Footer from "@/components/footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { motion } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import CustomFurniture from "../../public/images/1.jpg";
 import CustomFurniture_2 from "../../public/images/2.jpg";
 import CustomFurniture_3 from "../../public/images/3.jpg";
+import { performRequest } from "@/lib/datocms";
 
 export default function Home() {
+  const [pageContent, setPageContent] = useState(null);
   const targetRef = useRef(null);
   useEffect(() => {
     AOS.init({
       duration: 2000,
     });
   }, []);
+  // ---------------------------------------------------
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const query = `
+                      query {
+                        allServices(fallbackLocales: en, locale: en) {
+                          _allBtn2Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allBtnLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allBtn3Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allDesc2Locales(fallbackLocales: en, markdown: false) {
+                            locale
+                            value
+                          }
+                          _allDesc3Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allDescLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allDescriptionLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allHeaderLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allHorizontalTitle3Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allHorizontalTitle2Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allHorizontalTitleLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allTitle2Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allTitle3Locales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          _allTitleLocales(fallbackLocales: en) {
+                            locale
+                            value
+                          }
+                          btn(fallbackLocales: en, locale: en)
+                          btn2(locale: en, fallbackLocales: en)
+                          btn3(fallbackLocales: en, locale: en)
+                          desc(fallbackLocales: en, locale: en)
+                          desc2(locale: en, fallbackLocales: en)
+                          desc3(fallbackLocales: en, locale: en)
+                          description(fallbackLocales: en, locale: en)
+                          header(fallbackLocales: en, locale: en)
+                          horizontalTitle2(fallbackLocales: en, locale: en)
+                          horizontalTitle(fallbackLocales: en, locale: en)
+                          horizontalTitle3(fallbackLocales: en, locale: en)
+                          img {
+                            alt(locale: en, fallbackLocales: en)
+                            url
+                            title
+                            size
+                          }
+                          img2 {
+                            alt(fallbackLocales: en, locale: en)
+                            size
+                            url
+                            title
+                          }
+                          img3 {
+                            alt(fallbackLocales: en, locale: en)
+                            url
+                            title
+                          }
+                          title(fallbackLocales: en, locale: en)
+                          title2(fallbackLocales: en, locale: en)
+                          title3(fallbackLocales: en, locale: en)
+                        }
+                      }
+                  `;
+        const { data } = await performRequest({ query: query });
+
+        if (data.allServices) {
+          setPageContent(data?.allServices[0]);
+        } else {
+          console.error("Post not found");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+      }
+    }
+
+    fetchData();
+    // }
+  }, []);
+  console.log(pageContent);
+  // -------------------------------------------------------
   return (
     <Layout>
       <div className="bg-[#f9f1ec] flex justify-center items-center">
@@ -51,14 +168,11 @@ export default function Home() {
                   duration: 3,
                 }}
               >
-                Services
+                {pageContent?.header}
               </motion.p>
             </motion.section>
             <p className="text-[18px] text-[#957f72] font-light max-w-[512px] mt-[24px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique. Duis cursus,
-              mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
-              libero vitae erat.
+              {pageContent?.description}
             </p>
           </div>
           <div className="bg-[#f9f1ec] justify-center items-center">
@@ -68,7 +182,7 @@ export default function Home() {
                   <div className="col-span-1 " data-aos="fade-up">
                     <img
                       alt="author"
-                      src={CustomFurniture_2.src}
+                      src={pageContent?.img?.url}
                       className="mx-auto xl:mx-0 object-cover w-[100%] min-w-[239px] max-h-[502px] max-w-[502px]"
                       style={{ minWidth: "239px" }}
                       data-aos="slide-up"
@@ -80,7 +194,7 @@ export default function Home() {
                       style={{ fontFamily: "Playfair Display" }}
                       className="font-normal tracking-wider self-center text-[32px] sm:text-[46px] font-playfair text-[#4c4037] transform -rotate-90 whitespace-nowrap"
                     >
-                      Custom Furniture
+                      {pageContent?.horizontalTitle}
                     </span>
                   </div>
 
@@ -95,16 +209,13 @@ export default function Home() {
                       className="font-playfair text-[40px] text-[#4c4037]"
                       style={{ fontFamily: "Playfair Display" }}
                     >
-                      Details
+                      {pageContent?.title}
                     </p>
                     <p
                       className="font-playfair text-[18px] text-[#4c4037]"
                       style={{ fontFamily: "Manrope", maxWidth: "384px" }}
                     >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum nulla,
-                      ut commodo diam libero.
+                      {pageContent?.desc}
                     </p>
                     <p
                       className="font-playfair text-[10px] text-[#4c4037] border-b border-solid border-black py-[16px] mt-6 cursor-pointer"
@@ -114,7 +225,7 @@ export default function Home() {
                         letterSpacing: "3px",
                       }}
                     >
-                      GET IN TOUCH
+                      {pageContent?.btn}
                     </p>
                   </div>
                 </div>
@@ -124,7 +235,7 @@ export default function Home() {
                   <div className="col-span-1 " data-aos="fade-up">
                     <img
                       alt="author"
-                      src={CustomFurniture.src}
+                      src={pageContent?.img2?.url}
                       className="mx-auto xl:mx-0 object-cover w-[100%] min-w-[239px] max-h-[502px] max-w-[502px]"
                       style={{ minWidth: "239px" }}
                       data-aos="slide-up"
@@ -136,7 +247,7 @@ export default function Home() {
                       style={{ fontFamily: "Playfair Display" }}
                       className="font-normal tracking-wider self-center text-[32px] sm:text-[46px] font-playfair text-[#4c4037] transform -rotate-90 whitespace-nowrap"
                     >
-                      Furniture Layouts
+                      {pageContent?.horizontalTitle2}
                     </span>
                   </div>
 
@@ -151,16 +262,13 @@ export default function Home() {
                       className="font-playfair text-[40px] text-[#4c4037]"
                       style={{ fontFamily: "Playfair Display" }}
                     >
-                      Details
+                      {pageContent?.title2}
                     </p>
                     <p
                       className="font-playfair text-[18px] text-[#4c4037]"
                       style={{ fontFamily: "Manrope", maxWidth: "384px" }}
                     >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum nulla,
-                      ut commodo diam libero.
+                      {pageContent?.desc2}
                     </p>
                     <p
                       className="font-playfair text-[10px] text-[#4c4037] border-b border-solid border-black py-[16px] mt-6 cursor-pointer"
@@ -170,7 +278,7 @@ export default function Home() {
                         letterSpacing: "3px",
                       }}
                     >
-                      GET IN TOUCH
+                      {pageContent?.btn2}
                     </p>
                   </div>
                 </div>
@@ -180,7 +288,7 @@ export default function Home() {
                   <div className="col-span-1 " data-aos="fade-up">
                     <img
                       alt="author"
-                      src={CustomFurniture_3.src}
+                      src={pageContent?.img3?.url}
                       className="mx-auto xl:mx-0 object-cover w-[100%] min-w-[239px] max-h-[502px] max-w-[502px]"
                       style={{ minWidth: "239px" }}
                       data-aos="slide-up"
@@ -192,7 +300,7 @@ export default function Home() {
                       style={{ fontFamily: "Playfair Display" }}
                       className="font-normal tracking-wider self-center text-[32px] sm:text-[46px] font-playfair text-[#4c4037] transform -rotate-90 whitespace-nowrap"
                     >
-                      Space Planning
+                      {pageContent?.horizontalTitle3}
                     </span>
                   </div>
 
@@ -207,16 +315,13 @@ export default function Home() {
                       className="font-playfair text-[40px] text-[#4c4037]"
                       style={{ fontFamily: "Playfair Display" }}
                     >
-                      Details
+                      {pageContent?.title3}
                     </p>
                     <p
                       className="font-playfair text-[18px] text-[#4c4037]"
                       style={{ fontFamily: "Manrope", maxWidth: "384px" }}
                     >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum nulla,
-                      ut commodo diam libero.
+                      {pageContent?.desc3}
                     </p>
                     <p
                       className="font-playfair text-[10px] text-[#4c4037] border-b border-solid border-black py-[16px] mt-6 cursor-pointer"
@@ -226,7 +331,7 @@ export default function Home() {
                         letterSpacing: "3px",
                       }}
                     >
-                      GET IN TOUCH
+                      {pageContent?.btn3}
                     </p>
                   </div>
                 </div>
